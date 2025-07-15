46d397261e7752bc1e0f57ded87369d0cd641c47
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-15 13:50:58 +0500

    finding largest of three numbers

---
+++ Problem 22.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+
+if (num1>num2 and num1>num3):
+    print("Number1 is largest")
+
+elif (num2>num1 and num2>num3):
+    print("Number2 is largest")
+
+elif (num3>num1 and num3>num2):
+    print("Number3 is largest")
