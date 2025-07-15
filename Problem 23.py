6a100f27d44f3236daeebd46606eb53862968669
Waqas-Afridi-07 <afridiwaqas571@gmail.com>
2025-07-15 13:59:43 +0500

    checking if number is divisible by 5

---
+++ Problem 23.py
@@ -0,0 +1,10 @@
+num = int(input("Enter number: "))
+
+if(num<=0):
+    print("Please enter number greater than zero")
+
+elif(num%5==0):
+    print("Number is divisible by 5")
+
+else:
+    print("Number is not divisible by 5")
